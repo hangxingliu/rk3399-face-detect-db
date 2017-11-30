3ddf25bd6f0143ad26feaf78bdce73ec14376d53
LiuYue <hangxingliu@gmail.com>
2017-11-30 23:17:08 +0800

    Add `doxygen-docs` to absolute path checking range

--- scripts/check-no-absolute-path-git-hook.sh
+++ scripts/check-no-absolute-path-git-hook.sh
@@ -16,7 +16,6 @@ function check() {
 	find . -type f \
 		-not -path "./.git/*" \
 		-not -path "./build/*" \
-		-not -path "./docs/api-doxygen-docs/*"\
 		-not -path "${SELF}"\
 		| xargs -I __file__ grep __file__ -e "/home/"\
 			--with-filename --line-number\
