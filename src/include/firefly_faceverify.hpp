7a327085425192962f7d8e8ba0269a6e27abba49
LiuYue <hangxingliu@gmail.com>
2017-12-03 03:38:06 +0800

    Add firefly face sdk header files

---
+++ src/include/firefly_faceverify.hpp
@@ -0,0 +1,70 @@
+#include "./firefly_faceverify_version.hpp";
+#include "../public_types/firefly_face_sdk.hpp"
+
+#ifndef FIREFLY_FACEVERIFY_HPP
+#define FIREFLY_FACEVERIFY_HPP 1
+
+#define FF_OK						 0	///< 函数返回成功
+#define FF_E_ILLEGAL_ARGUMENT		-1	///< 非法参数
+#define FF_E_INVALID_HANDLE			-2	///< 无效句柄
+#define FF_E_NO_MEMORY				-3	///< 内存不足
+#define FF_E_FAIL					-4	///< 内部错误
+#define FF_E_INCOMPATIBLE_VERSION	-5  ///< 版本不兼容
+#define FF_E_BAD_CONFIG_FILE		-6  ///< 配置文件格式错误
+#define FF_E_FILE_NOT_FOUND			-7  ///< 找不到文件
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+#    ifdef FF_EXPORTS
+#        define FF_API  extern "C" __attribute__((visibility ("default")))
+#    else
+#        define FF_API extern "C"
+#    endif
+#else
+#    ifdef FF_EXPORTS
+#        define FF_API __attribute__((visibility ("default")))
+#    else
+#        define FF_API
+#    endif
+#endif
+
+FF_API FF_Detector firefly_fd_init_detector();
+
+FF_API void firefly_fd_deinit_detector(const FF_Verifier detector_handle);
+
+FF_API int firefly_fd_detect_image(
+	const FF_Detector detector_handle,
+    const FF_Image image,
+    FF_FaceInfo **p_faces_array,
+	int *p_faces_count);
+
+FF_API void firefly_fd_free_detect_result(const FF_FaceInfo* faces_array);
+
+FF_API int firefly_fd_get_version();
+
+FF_API FF_Verifier firefly_fv_init_verifier();
+
+FF_API void firefly_fv_deinit_verifier(const FF_Verifier verifier_handle);
+
+FF_API int firefly_fv_extract_feature(
+	const FF_Verifier verifier_handle,
+    const FF_Image image,
+	const FF_FaceInfo face,
+	FF_FaceFeatures* p_feature);
+
+FF_API float firefly_fv_compare_feature(
+	FF_FaceFeatures& feature1,
+	FF_FaceFeatures& feature2);
+
+FF_API int firefly_fv_get_feature_length(const FF_Verifier verifier);
+
+FF_API int firefly_fv_get_version();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/public_types/firefly_face_sdk.hpp
@@ -0,0 +1,79 @@
+#ifndef PUBLIC_TYPES_FIREFLY_FACE_HPP
+#define PUBLIC_TYPES_FIREFLY_FACE_HPP 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void * FF_Detector;
+typedef void * FF_Verifier;
+
+typedef enum {
+	FF_IMAGE_BGR8UC3 = 0,	///< BGR  8:8:8   24bpp ( 3通道24bit BGR 像素 )
+	FF_IMAGE_BGRA8UC4 = 1,	///< BGRA 8:8:8:8 32bpp ( 4通道32bit BGRA 像素 )
+	FF_IMAGE_GRAY8UC1 = 2,	///< Y    1        8bpp ( 单通道8bit灰度像素 )
+	FF_IMAGE_MAX
+} FF_ImageType;
+
+
+/**
+ * Firefly Face SDK Rect(left, top, right, bottom)
+ */
+typedef struct FF_Rect {
+	int left;	///< 矩形左边的坐标
+	int top;	///< 矩形顶边的坐标
+	int right;	///< 矩形右边的坐标
+	int bottom;	///< 矩形底边的坐标
+} FF_Rect;
+
+
+/**
+ * Firefly Face SDK Image(OpenCVImageData, imageType, w, h)
+ */
+typedef struct FF_Image {
+	unsigned char *data;
+	FF_ImageType type;
+	int width;
+	int height;
+} FF_Image;
+
+/**
+ * Firefly Face SDK Point(x, y)
+ */
+typedef struct FF_Point {
+	float x;
+	float y;
+} FF_Point;
+
+/**
+ * Firefly Face SDK Face Info(rect, confidence, landmarks[5],  y,p,r, id, quality )
+ */
+typedef struct FF_FaceInfo {
+	FF_Rect rect;
+	/** chinese: 置信度 */
+	float confidence;
+	FF_Point landmarks[5];
+
+	int yaw;
+	int pitch;
+	int roll;
+
+	int id;
+
+	float quality;
+} FF_FaceInfo;
+
+#define FF_FACE_FEATURE_LEN 6000
+/**
+ * Firefly Face SDK Face Features(len, data[6000])
+ */
+typedef struct FF_FaceFeatures {
+	int len;
+	float data[FF_FACE_FEATURE_LEN];
+} FF_FaceFeatures;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
