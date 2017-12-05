0fff730709173f1e97ae15f247b1fdd6751fc616
LiuYue <hangxingliu@gmail.com>
2017-12-06 06:23:25 +0800

    modify capture.hpp for structs

--- src/public_types/capture.hpp
+++ src/public_types/capture.hpp
@@ -1,48 +1,63 @@
-#include "./firefly_face_sdk.hpp"
 #include "./database.hpp"
+#include "./firefly_face_sdk.hpp"
 
 #ifndef PUBLIC_TYPES_CAPTURE_HPP
 #define PUBLIC_TYPES_CAPTURE_HPP
 
-#define MAX_FACE_IN_ONE_CAPTURE_FRAME 5
+#define MAX_PERSON_IN_ONE_CAPTURE_FRAME 5
 
 typedef enum {
 	/// Channel order: Red Green Blue
-	IMAGE_FORMAT_RGB = 1,
+	CAPTURE_IMAGE_FORMAT_RGB = 1,
 	/// Channel order: Blue Green Red (**OpenCV default**)
-	IMAGE_FORMAT_BGR = 2
+	CAPTURE_IMAGE_FORMAT_BGR = 2
 } CaptureImageFormat;
+
 
 /**
  * @see getFrameFromCapture()
  */
 typedef struct CaptureRequestOptions {
 
-	CaptureImageFormat imageFormat;
+	CaptureImageFormat imageFormat = CAPTURE_IMAGE_FORMAT_RGB;
 
 	/** face detection will be happen in each request if this value equals **1** */
 	int disableMotionDetection = 0;
 
-	/** set this value as 1 if you want **allocate memory of `image.data` by yourself** */
+	/** set this value to 1 if you want **allocate memory of `image.data` by yourself** */
 	int disableMallocImageDataArray = 0;
+
+	int disableFaceDetection = 0;
 
 } CaptureRequestOptions;
 
+
 typedef struct CaptureFrameImage {
 	CaptureImageFormat format;
-	unsigned char* data;
-	int dataLength;
-	int w;
-	int h;
+	unsigned char* data = nullptr;
+	int dataLength = 0;
+	int w = 0;
+	int h = 0;
 } CaptureFrameImage;
+
+
+typedef struct CapturePersonInfo_Pointf {
+	float x = 0.0f, y = 0.0f;
+} CapturePersonInfo_Pointf;
+
 
 typedef struct CapturePersonInfo {
 
-	FF_FaceInfo info;
+	/// Face rect: (x0, y0)~(x1, y1)
+	int x0 = 0, y0 = 0, x1 = 0, y1 = 0;
+	float confidence;
+	float quality;
+	int yaw, pitch, roll;
+	CapturePersonInfo_Pointf landmarks[5];
 
-	/** (0:not/1:got) is program got this feature info from this face */
-	int gotFeatures = 0;
-	FF_FaceFeatures features;
+	/** 0: could not get face features */
+	int featureLength = 0;
+	float features[FF_FACE_FEATURE_LEN];
 
 	/** (0:not/1:matched) is this person info matched a person in database*/
 	int matched = 0;
@@ -54,20 +69,19 @@ typedef struct CapturePersonInfo {
 } CapturePersonInfo;
 typedef CapturePersonInfo* CapturePersonInfoArray;
 
+
 /**
  * @see getFrameFromCapture()
  */
 typedef struct CaptureFrameAndPersonInfo {
 	CaptureFrameImage image;
 
-	int faceCount = 0;
-
 	/**
-	 * **Maybe NULL** it is null when (faceCount == 0) is true
-	 * Max length: 5 MAX_FACE_IN_ONE_CAPTURE_FRAME
-	 * @see MAX_FACE_IN_ONE_CAPTURE_FRAME
+	 * Max: 5 MAX_PERSON_IN_ONE_CAPTURE_FRAME
+	 * @see MAX_PERSON_IN_ONE_CAPTURE_FRAME
 	 */
-	CapturePersonInfoArray person;
+	int personCount = 0;
+	CapturePersonInfoArray persons = nullptr;
 
 } CaptureFrameAndFaceInfo;
 
