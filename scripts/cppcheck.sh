906910dcfaebb7b4cacc9d52dd3d2ae6811bd619
LiuYue <hangxingliu@gmail.com>
2017-12-07 21:57:54 +0800

    Cppcheck script. Doxygen file. VSCode cpp file.

---
+++ scripts/cppcheck.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# get directory this script located in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )";
+	SOURCE="$(readlink "$SOURCE")"; [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE";
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR/..;
+
+GREEN="\x1b[1;32m";
+RED="\x1b[1;31m";
+YELLOW="\x1b[1;33m";
+BLUE="\x1b[1;34m";
+RESET="\x1b[0m";
+function fatal() { echo -e "\n  ${RED}error: $1$RESET\n"; exit 1; }
+function colorable() {
+	awk \
+		-vERROR="$RED"\
+		-vWARNING="$YELLOW"\
+		-vNORMAL="\x1b[33m"\
+		-vIGNORE="\x1b[90m"\
+		-vRESET="$RESET"\
+		'$2 == "(error)"{ print ERROR $0 RESET; next;}
+		$2 == "(warning)"{ print WARNING $0 RESET; next;}
+		/never used/ || /Unused/ { print IGNORE $0 RESET; next; }
+		{ print NORMAL $0 RESET;}';
+}
+
+CPPCHECK="cppcheck";
+[[ -z `which $CPPCHECK` ]] && fatal "missing \"$CPPCHECK\" ";
+
+[[ -n `echo "$*" | awk '/test/'` ]] && PROJ="." || PROJ="src";
+$CPPCHECK "$PROJ" --enable=all --quiet --force 2>&1 | colorable;
